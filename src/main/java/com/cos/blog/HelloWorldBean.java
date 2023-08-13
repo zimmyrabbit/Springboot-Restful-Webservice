3bd005eb8bd1fa540edd48f1ae755c26e7ae7b51
LAPTOP-UF7DLA8Q\zimmy <zimmyrabbit@naver.com>
2023-08-13 18:26:19 +0900

    javaBean Return api 생성

---
+++ src/main/java/com/cos/blog/HelloWorldBean.java
@@ -0,0 +1,26 @@
+package com.cos.blog;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//lombok
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloWorldBean {
+	private String message;
+
+// @Data
+//	public String getMessage() {
+//		return this.message;
+//	}
+//	public void setMessage(String msg) {
+//		this.message = msg;
+//	}
+	
+// @AllArgsConstructor
+//	public HelloWorldBean(String message) {
+//		this.message = message;
+//	}
+}
--- src/main/java/com/cos/blog/HelloWorldController.java
+++ src/main/java/com/cos/blog/HelloWorldController.java
@@ -12,4 +12,9 @@ public class HelloWorldController {
 	public String helloWorld() {
 		return "Hello World";
 	}
+	
+	@GetMapping(path="/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World");
+	}
 }
