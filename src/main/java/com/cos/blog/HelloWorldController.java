36d81e0343458a1377119993c0ebe3dedcf7c3cd
LAPTOP-UF7DLA8Q\zimmy <zimmyrabbit@naver.com>
2023-08-13 18:39:34 +0900

    path variable 사용

--- src/main/java/com/cos/blog/HelloWorldController.java
+++ src/main/java/com/cos/blog/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.cos.blog;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,5 +17,10 @@ public class HelloWorldController {
 	@GetMapping(path="/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("Hello World");
+	}
+	
+	@GetMapping(path="/hello-world-bean/path-variable/{name}")
+	public HelloWorldBean helloWorldBean(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World, %s", name));
 	}
 }
