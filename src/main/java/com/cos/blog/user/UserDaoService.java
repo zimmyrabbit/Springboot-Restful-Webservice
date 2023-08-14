c2fbf0190df35128558fd47c014287801a834d4c
LAPTOP-UF7DLA8Q\zimmy <zimmyrabbit@naver.com>
2023-08-14 22:01:10 +0900

    GET HTTP Method

--- src/main/java/com/cos/blog/user/UserDaoService.java
+++ src/main/java/com/cos/blog/user/UserDaoService.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserDaoService {
 	private static List<User> users = new ArrayList<>();
 	
