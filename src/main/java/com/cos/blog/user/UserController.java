f981f0cb07ec5eebf7d28d75fa507f750714eaad
LAPTOP-UF7DLA8Q\zimmy <zimmyrabbit@naver.com>
2023-08-15 20:36:49 +0900

    HTTP Status Code 제어

--- src/main/java/com/cos/blog/user/UserController.java
+++ src/main/java/com/cos/blog/user/UserController.java
@@ -1,12 +1,15 @@
 package com.cos.blog.user;
 
+import java.net.URI;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserController {
@@ -27,7 +30,14 @@ public class UserController {
 	}
 	
 	@PostMapping("/users")
-	public void createUser(@RequestBody User user) {
+	public ResponseEntity<User> createUser(@RequestBody User user) {
 		User savedUser = service.save(user);
+		
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+			.path("/{id}")
+			.buildAndExpand(savedUser.getId())
+			.toUri();
+		
+		return ResponseEntity.created(location).build();
 	}
 }
