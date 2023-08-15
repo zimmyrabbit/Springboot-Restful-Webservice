685935a028168589ce0596293da1c1969719d298
LAPTOP-UF7DLA8Q\zimmy <zimmyrabbit@naver.com>
2023-08-15 20:30:57 +0900

    POST HTTP Method

--- src/main/java/com/cos/blog/user/UserController.java
+++ src/main/java/com/cos/blog/user/UserController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,5 +24,10 @@ public class UserController {
 	@GetMapping("/users/{id}")
 	public User retrieveUser(@PathVariable int id) {
 		return service.findOne(id);
+	}
+	
+	@PostMapping("/users")
+	public void createUser(@RequestBody User user) {
+		User savedUser = service.save(user);
 	}
 }
