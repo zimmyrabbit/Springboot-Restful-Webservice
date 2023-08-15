58d693689acef2f28f759b1b119b301d17e43c9e
LAPTOP-UF7DLA8Q\zimmy <zimmyrabbit@naver.com>
2023-08-15 21:02:03 +0900

    DELETE HTTP Method

--- src/main/java/com/cos/blog/user/UserController.java
+++ src/main/java/com/cos/blog/user/UserController.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,5 +48,14 @@ public class UserController {
 			.toUri();
 		
 		return ResponseEntity.created(location).build();
+	}
+	
+	@DeleteMapping("/users/{id}")
+	public void deleteUser(@PathVariable int id) {
+		User user = service.deleteById(id);
+		
+		if (user == null) {
+			throw new UserNotFoundException(String.format("ID [%s] not found", id));
+		}
 	}
 }
