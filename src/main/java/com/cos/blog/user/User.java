d4662e7c1fe9f47093d470a601bd489bf046b08c
LAPTOP-UF7DLA8Q\zimmy <zimmyrabbit@naver.com>
2023-08-16 22:13:51 +0900

    유효성 체크를 위한 Validation API 사용

--- src/main/java/com/cos/blog/user/User.java
+++ src/main/java/com/cos/blog/user/User.java
@@ -2,6 +2,9 @@ package com.cos.blog.user;
 
 import java.util.Date;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -9,6 +12,9 @@ import lombok.Data;
 @AllArgsConstructor
 public class User {
 	private Integer id;
+	
+	@Size(min=2, message="Name은 2글자 이상 입력해 주세요.")
 	private String name;
+	@Past
 	private Date joinDate;
 }
--- src/main/java/com/cos/blog/user/UserController.java
+++ src/main/java/com/cos/blog/user/UserController.java
@@ -3,6 +3,8 @@ package com.cos.blog.user;
 import java.net.URI;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -39,7 +41,7 @@ public class UserController {
 	}
 	
 	@PostMapping("/users")
-	public ResponseEntity<User> createUser(@RequestBody User user) {
+	public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
 		User savedUser = service.save(user);
 		
 		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
