3abe0362609b7ca3cc9b94324094166f60ef83d6
LAPTOP-UF7DLA8Q\zimmy <zimmyrabbit@naver.com>
2023-08-16 22:30:39 +0900

    다국어 처리를 위한 Internationalization 구현

--- src/main/java/com/cos/blog/RestfulWebServiceApplication.java
+++ src/main/java/com/cos/blog/RestfulWebServiceApplication.java
@@ -1,7 +1,13 @@
 package com.cos.blog;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 public class RestfulWebServiceApplication {
@@ -9,5 +15,19 @@ public class RestfulWebServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestfulWebServiceApplication.class, args);
 	}
+	
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.KOREA);
+		return localeResolver;
+	}
+	
+	@Bean(name="messageSource")
+	public ResourceBundleMessageSource bundleMessageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource;
+	} 
 
 }
