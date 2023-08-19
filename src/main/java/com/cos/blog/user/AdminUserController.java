5b03f6f7340d3881c88a731fe4a30ee2bba35f55
LAPTOP-UF7DLA8Q\zimmy <zimmyrabbit@naver.com>
2023-08-19 23:31:11 +0900

    mime type을 이용한 Restapi 버전 관리

--- src/main/java/com/cos/blog/user/AdminUserController.java
+++ src/main/java/com/cos/blog/user/AdminUserController.java
@@ -41,7 +41,8 @@ public class AdminUserController {
 	// /admin/users/1 -> /admin/v1/users/1
 //	@GetMapping("/v1/users/{id}")
 //	@GetMapping(value="/users/{id}", params="version=1")
-	@GetMapping(value="/users/{id}", headers="X-API-VERSION=1")
+//	@GetMapping(value="/users/{id}", headers="X-API-VERSION=1")
+	@GetMapping(value="/users/{id}", produces="application/vnd.company.appv1+json")
 	public MappingJacksonValue retrieveUserV1(@PathVariable int id) {
 		//return service.findOne(id);
 		
@@ -64,7 +65,8 @@ public class AdminUserController {
 	
 //	@GetMapping("/v2/users/{id}")
 //	@GetMapping(value="/users/{id}", params="version=2")
-	@GetMapping(value="/users/{id}", headers="X-API-VERSION=2")
+//	@GetMapping(value="/users/{id}", headers="X-API-VERSION=2")
+	@GetMapping(value="/users/{id}", produces="application/vnd.company.appv2+json")
 	public MappingJacksonValue retrieveUserV2(@PathVariable int id) {
 		//return service.findOne(id);
 		
