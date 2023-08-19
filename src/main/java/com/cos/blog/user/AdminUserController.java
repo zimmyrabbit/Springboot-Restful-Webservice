589817e2727dee31204576e181f8f9bb23daf65a
LAPTOP-UF7DLA8Q\zimmy <zimmyrabbit@naver.com>
2023-08-19 23:21:44 +0900

    URI를 이용한 RestApi 버전 관리

--- src/main/java/com/cos/blog/user/AdminUserController.java
+++ src/main/java/com/cos/blog/user/AdminUserController.java
@@ -2,6 +2,7 @@ package com.cos.blog.user;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -37,8 +38,9 @@ public class AdminUserController {
 		return mapping;
 	}
 	
-	@GetMapping("/users/{id}")
-	public MappingJacksonValue retrieveUser(@PathVariable int id) {
+	// /admin/users/1 -> /admin/v1/users/1
+	@GetMapping("/v1/users/{id}")
+	public MappingJacksonValue retrieveUserV1(@PathVariable int id) {
 		//return service.findOne(id);
 		
 		User user = service.findOne(id);
@@ -53,6 +55,31 @@ public class AdminUserController {
 		FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo", filter);
 		
 		MappingJacksonValue mapping = new MappingJacksonValue(user);
+		mapping.setFilters(filters);
+		
+		return mapping;
+	}
+	
+	@GetMapping("/v2/users/{id}")
+	public MappingJacksonValue retrieveUserV2(@PathVariable int id) {
+		//return service.findOne(id);
+		
+		User user = service.findOne(id);
+		
+		if(user == null) {
+			throw new UserNotFoundException(String.format("ID [%s] not found", id));
+		}
+		
+		UserV2 userV2 = new UserV2();
+		BeanUtils.copyProperties(user, userV2);
+		userV2.setGrade("VIP");
+		
+		SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter
+				.filterOutAllExcept("id","name","joinDate","grade");
+		
+		FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfoV2", filter);
+		
+		MappingJacksonValue mapping = new MappingJacksonValue(userV2);
 		mapping.setFilters(filters);
 		
 		return mapping;
--- src/main/java/com/cos/blog/user/UserV2.java
+++ src/main/java/com/cos/blog/user/UserV2.java
@@ -11,23 +11,14 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 //@JsonIgnoreProperties(value= {"password","ssn"})
-@JsonFilter("UserInfo")
-public class User {
-	private Integer id;
-	
-	@Size(min=2, message="Name은 2글자 이상 입력해 주세요.")
-	private String name;
-	@Past
-	private Date joinDate;
-	
-//	@JsonIgnore
-	private String password;
-//	@JsonIgnore
-	private String ssn;
+@JsonFilter("UserInfoV2")
+public class UserV2 extends User{
+	private String grade;
 }
