177587cdac544036bee4ac4f8ac6052a9e91b694
LAPTOP-UF7DLA8Q\zimmy <zimmyrabbit@naver.com>
2023-08-19 23:24:51 +0900

    ReqeustParameter를 이용한 RestApi 버전 관리

--- src/main/java/com/cos/blog/user/AdminUserController.java
+++ src/main/java/com/cos/blog/user/AdminUserController.java
@@ -39,7 +39,8 @@ public class AdminUserController {
 	}
 	
 	// /admin/users/1 -> /admin/v1/users/1
-	@GetMapping("/v1/users/{id}")
+//	@GetMapping("/v1/users/{id}")
+	@GetMapping(value="/users/{id}", params="version=1")
 	public MappingJacksonValue retrieveUserV1(@PathVariable int id) {
 		//return service.findOne(id);
 		
@@ -60,7 +61,8 @@ public class AdminUserController {
 		return mapping;
 	}
 	
-	@GetMapping("/v2/users/{id}")
+//	@GetMapping("/v2/users/{id}")
+	@GetMapping(value="/users/{id}", params="version=2")
 	public MappingJacksonValue retrieveUserV2(@PathVariable int id) {
 		//return service.findOne(id);
 		
